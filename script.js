e5ae0bbda229b571cb6b71d9e3063a429510b599
Colin <colind2824@tuckahoeschools.org>
2025-01-31 13:24:58 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//This function appends the value passed to it (val) to the current value of an input field with the id 'res'.
 function Solve(val) {
     const v = document.getElementById('res');
     v.value += val;
  }
+ //This code takes a mathematical expression from an input field with the id 'res', replaces 'x' with '*'. 
+ // Evaluates the expression, and then updates the input field with the result. 
+ // If there's an error during evaluation, it sets the input field value to 'Error'.
  function Result() {
     const num1 = document.getElementById('res').value;
     try {
@@ -21,6 +25,11 @@ function Solve(val) {
     var ev = document.getElementById('res');
     ev.value = ev.value.slice(0, -1);
  }
+ // The code below  listens for keydown events and performs different actions based on the key pressed://
+//If the key is a digit or one of the operators (+, -, *, /, ., %), it calls the Solve function, replacing * with x.//
+//If the key is Enter, it calls the Result function.//
+//If the key is Backspace, it calls the Back function.//
+//If the key is c or C, it calls the Clear function.//
  document.addEventListener('keydown', function (event) {
     const key = event.key;
     const validKeys = '0123456789+-*/.%';
